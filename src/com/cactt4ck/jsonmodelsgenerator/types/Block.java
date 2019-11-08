0f40d06552c8ba82145d2f393294eb9e707eda35
CacTt4ck <lachainedelightning13090@gmail.com>
2019-11-08 14:33:37 +0100

    commit

--- src/com/cactt4ck/jsonmodelsgenerator/types/Block.java
+++ src/com/cactt4ck/jsonmodelsgenerator/types/Block.java
@@ -2,6 +2,7 @@ package com.cactt4ck.jsonmodelsgenerator.types;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -9,7 +10,6 @@ import java.nio.file.Paths;
 
 public class Block {
 
-    private File blockState, model;
     private String name;
 
     public Block(String name) {
@@ -29,20 +29,18 @@ public class Block {
         modelJson.put("parent", "block/cube_all");
 
 
-
-
-        try (FileWriter file = new FileWriter("D:\\blockstate.json")) {
-            file.write(blockstateJson.toJSONString());
+        try (FileWriter file = new FileWriter("blockstate.json")) {
+            file.write(blockstateJson.toJSONString().replace("\\", ""));
             System.out.println("Successfully Copied JSON Object to File...");
-            System.out.println("\nJSON Object: " + blockstateJson);
+            System.out.println("\nJSON Object: " + blockstateJson.toJSONString().replace("\\", ""));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        try (FileWriter file = new FileWriter("D:\\model.json")) {
-            file.write(modelJson.toJSONString());
+        try (FileWriter file = new FileWriter("model.json")) {
+            file.write(modelJson.toJSONString().replace("\\", ""));
             System.out.println("Successfully Copied JSON Object to File...");
-            System.out.println("\nJSON Object: " + modelJson);
+            System.out.println("\nJSON Object: " + blockstateJson.toJSONString().replace("\\", ""));
         } catch (IOException e) {
             e.printStackTrace();
         }
